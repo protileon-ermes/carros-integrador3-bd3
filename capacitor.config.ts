6c756b5f3e483424139195b2d47d1696dfefcbee
Ermindo Treviso Neto <ermindone@gmail.com>
2024-07-22 17:47:49 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'integrador',
+  webDir: 'www'
+};
+
+export default config;
